43f265e7ed7ee7ecfc8e0e8cc4a0b2952c942173
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2026-01-17 08:42:26 +0900

    mutex version

--- cliquery/query.go
+++ cliquery/query.go
@@ -13,6 +13,7 @@ import (
 func main() {
 	var nClient = 50
 	var nCount = 1000
+	var start = time.Now()
 
 	db, err := machcli.NewDatabase(&machcli.Config{
 		Host:         "127.0.0.1",
@@ -34,72 +35,74 @@ func main() {
 	if result.Err() != nil {
 		panic(result.Err())
 	}
+
+	for j := 0; j < 200; j++ {
+		result := conn.Exec(ctx, "INSERT INTO tag(name, time, value) VALUES('tag1', now, 123.45)")
+		if result.Err() != nil {
+			panic(result.Err())
+		}
+	}
 	conn.Close()
+
+	mu := sync.Mutex{}
 
 	var wg sync.WaitGroup
 	for i := 0; i < nClient; i++ {
 		wg.Add(1)
-		if i == 0 {
-			go func(ctx context.Context, clientId int) {
-				defer wg.Done()
-				conn, err := db.Connect(ctx, api.WithPassword("sys", "manager"))
+
+		go func(ctx context.Context, clientId int) {
+			defer wg.Done()
+			var conn *machcli.Conn
+			mu.Lock()
+			if c, err := db.Connect(ctx, api.WithPassword("sys", "manager")); err != nil {
+				panic(err)
+			} else {
+				conn = c.(*machcli.Conn)
+			}
+			mu.Unlock()
+			defer func() {
+				mu.Lock()
+				conn.Close()
+				mu.Unlock()
+			}()
+			for j := 0; j < nCount; j++ {
+				tick := time.Now()
+				mu.Lock()
+				r, err := conn.Query(ctx, "SELECT * FROM tag WHERE name='tag1' LIMIT 100")
+				mu.Unlock()
+				if err != nil {
+					fmt.Printf("client %d, elapsed %v\n", clientId, time.Since(tick))
+					panic(err)
+				}
+				rows := r.(*machcli.Rows)
+				n := 0
+				for rows.Next() {
+					if err := rows.Err(); err != nil {
+						panic(err)
+					}
+					n++
+					var name string
+					var t time.Time
+					var v float64
+					if err := rows.Scan(&name, &t, &v); err != nil {
+						panic(err)
+					}
+					if name != "tag1" {
+						panic(fmt.Sprintf("invalid name: %s", name))
+					}
+				}
+				if n != 100 {
+					panic(fmt.Sprintf("invalid row count: %d", n))
+				}
+				mu.Lock()
+				err = rows.Close()
+				mu.Unlock()
 				if err != nil {
 					panic(err)
 				}
-				for j := 0; j < nCount; j++ {
-					result := conn.Exec(ctx, "INSERT INTO tag(name, time, value) VALUES('tag1', now, 123.45)")
-					if result.Err() != nil {
-						panic(result.Err())
-					}
-				}
-				defer conn.Close()
-			}(ctx, i)
-		} else {
-			go func(ctx context.Context, clientId int) {
-				defer wg.Done()
-				var conn *machcli.Conn
-				// if c, err := db.Connect(ctx, api.WithPassword("sys", "manager")); err != nil {
-				// 	panic(err)
-				// } else {
-				// 	conn = c.(*machcli.Conn)
-				// }
-				// defer conn.Close()
-
-				for j := 0; j < nCount; j++ {
-					if c, err := db.Connect(ctx, api.WithPassword("sys", "manager")); err != nil {
-						panic(err)
-					} else {
-						conn = c.(*machcli.Conn)
-					}
-
-					tick := time.Now()
-					r, err := conn.Query(ctx, "SELECT * FROM tag WHERE name='tag1' LIMIT 100, 100")
-					if err != nil {
-						fmt.Printf("client %d, elapsed %v\n", clientId, time.Since(tick))
-						panic(err)
-					}
-					rows := r.(*machcli.Rows)
-					n := 0
-					for rows.Next() {
-						if err := rows.Err(); err != nil {
-							panic(err)
-						}
-						n++
-					}
-					if n != 100 {
-						panic(fmt.Sprintf("invalid row count: %d", n))
-					}
-					err = rows.Close()
-					if err != nil {
-						panic(err)
-					}
-
-					if err := conn.Close(); err != nil {
-						panic(err)
-					}
-				}
-			}(ctx, i)
-		}
+			}
+		}(ctx, i)
 	}
 	wg.Wait()
+	fmt.Printf("All clients completed in %v  %d ops/sec\n", time.Since(start), int(float64(nClient*nCount)/time.Since(start).Seconds()))
 }
